b4c04b2b79b2190d66212f9ba098599b289503d8
tfRaraShi99 <sutrishnadn99@gmail.com>
2024-12-13 01:24:21 +0530

    Nineth Commit: Model Trainer file is been modified

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,6 +5,8 @@ from src.logger import logging
 
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
 
 import pandas as pd 
 from sklearn.model_selection import train_test_split
@@ -50,4 +52,9 @@ if __name__=='__main__':
     train_data, test_data = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)    
+    train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data,test_data) 
+
+    modeltrainer = ModelTrainer()
+    r2_score = modeltrainer.initiate_model_trainer(train_arr,test_arr)
+    print(f'r2_score : {r2_score}')
+
